2ec13cce8e8482390abe99bc7f35f70ea9fb6dde
alexistvantoth <alex.istvan.toth@gmail.com>
2024-06-08 18:32:17 +0200

    init

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+
+@WebSocketGateway()
+export class ChatGateway {
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): string {
+    return 'Hello world!';
+  }
+}
